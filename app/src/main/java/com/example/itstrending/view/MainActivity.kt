0a2e27e3d9adbc8f311fcc381d3775eec516ee13
Meghana <meghana.basutkar@arvindinternet.com>
2021-08-09 18:26:05 +0530

    progress added

--- app/src/main/java/com/example/itstrending/view/MainActivity.kt
+++ app/src/main/java/com/example/itstrending/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.itstrending.view
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,11 +13,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModel: TrendingViewModel
     lateinit var recyclerview: RecyclerView
     lateinit var mainAdapter: TrendingReposAdapter
+    lateinit var progress: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.itstrending.R.layout.activity_main)
         recyclerview = findViewById(com.example.itstrending.R.id.recyclerView)
+        progress = findViewById(com.example.itstrending.R.id.progressBar)
         viewModel = ViewModelProvider(this).get(TrendingViewModel::class.java)
         setUpRecycler()
     }
@@ -24,16 +28,17 @@ class MainActivity : AppCompatActivity() {
         //defining layout manager
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
-        mainAdapter = TrendingReposAdapter(viewModel,  this)
+        mainAdapter = TrendingReposAdapter(viewModel, this)
         recyclerview.apply {
             this.layoutManager = layoutManager
             this.adapter = mainAdapter
         }
+        progress.visibility = View.VISIBLE
         observeChangesInList()
     }
 
     private fun observeChangesInList() {
-        //todo: add loader & pull to refresh
-        viewModel.fetchTrendingReposResponse().observe(this, { mainAdapter.setList(it) })
+        viewModel.fetchTrendingReposResponse().observe(this, { mainAdapter.setList(it)
+        progress.visibility = View.GONE})
     }
 }
